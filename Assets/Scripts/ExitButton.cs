8e54ee2c076f7760beab7ad44cf6b55932a2c716
enpl <nnpl22@gmail.com>
2017-11-23 21:55:39 +0900

    unity-report
    d

---
+++ Assets/Scripts/ExitButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButton : MonoBehaviour {
+
+    private GameObject exitBtn;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+	
+	
+}
